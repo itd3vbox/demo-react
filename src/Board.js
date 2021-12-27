59d48e55e946b3d7eb2e7b7a0ba349cca827accb
Anonymous Box <it.d3vbox@gmail.com>
2021-12-27 19:19:48 +0100

    tuto "Adding Time Travel" done

--- src/Board.js
+++ src/Board.js
@@ -1,50 +1,20 @@
 import React from 'react'
 import Square from './Square'
-import { calculateWinner } from './Helper'
 
 export default class Board extends React.Component 
 {
-    constructor(props)
-    {
-        super(props)
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-            winner: null, 
-        }
-    }
-
-    handleClick(i)
-    {
-        const squares = this.state.squares.slice()
-
-        const cannotClick = calculateWinner(squares) || squares[i]
-        if(cannotClick) return
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O'
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        })
-    }
-
     renderSquare(i) 
     {
         return <Square 
-            value={ this.state.squares[i] } 
-            onClick={ () => this.handleClick(i) }
+            value={ this.props.squares[i] } 
+            onClick={ () => this.props.onClick(i) }
             />
     }
 
     render() 
     {
-        const winner = calculateWinner(this.state.squares)
-        const status = winner ? `Winner: ${ winner }`
-            : `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
